c6db9dbf71f3ccaad8c0d157fb67cae3031ab11c
javier <javier@35f86111-0fec-4b18-bb87-2eeb345c1b12>
2009-10-22 13:54:38 +0000

    add mp4 to video filter

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -3,7 +3,7 @@ pref("extensions.foneradownloader.foneraip", "fonera");
 pref("extensions.foneradownloader.enabled", true);
 pref("extensions.foneradownloader.onwan", false);
 pref("extensions.foneradownloader.filters.images", "\.jpg$|\.png$|\.gif$");
-pref("extensions.foneradownloader.filters.movies", "\.avi$|\.mov$|\.mkv$|\.ogv$");
+pref("extensions.foneradownloader.filters.movies", "\.avi$|\.mp4$|\.mov$|\.mkv$|\.ogv$");
 pref("extensions.foneradownloader.filters.music", "\.mp3$|\.wma$|\.ogg$|\.acc$|\.flac$");
 pref("extensions.foneradownloader.filters.torrents", "\.torrent$");
 pref("extensions.foneradownloader.filters.megaupload", "megaupload");
